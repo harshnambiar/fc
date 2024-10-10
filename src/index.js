aacabe6b66dd9ff1c611bea04b9b6907e11bdd44
harshnambiar <harrisonlvl2@gmail.com>
2024-10-10 16:38:21 +0530

    added create page

--- src/index.js
+++ src/index.js
@@ -122,9 +122,9 @@ async function createCapsule() {
   
   const myAddress = localStorage.getItem("acc");
   const pay = web3.utils.toWei('0.01', 'ether');
-  const factor = 2;
+  const factor = 5;
   try {
-    const res = await contract.methods.storeAmount("0xD0dC8A261Ad1B75A92C5e502AE10c3Fde042b879", 4, factor, "welcome").send({from: myAddress, value: factor * pay, gas: 100000, gasLimit: 1000000});
+    const res = await contract.methods.storeAmount("0xD0dC8A261Ad1B75A92C5e502AE10c3Fde042b879", 9, factor, "sort test").send({from: myAddress, value: factor * pay, gas: 100000, gasLimit: 1000000});
     document.getElementById("res").innerHTML = `
     Result:
     <br/>
@@ -171,10 +171,11 @@ async function distributeCapsules() {
   }
   catch (err){
     console.log(err);
+    const e = "The function has already been called today";
     document.getElementById("res").innerHTML = `
     Result:
     <br/>
-    `.concat(err.toString()).concat(`
+    `.concat(e).concat(`
   `);
   }
 
@@ -196,6 +197,102 @@ async function to_capsules(){
   window.location.href = "./capsules.html";
 }
 window.to_capsules = to_capsules;
+
+async function to_create(){
+  window.location.href = "./create.html";
+}
+window.to_create = to_create;
+
+async function load_create(){
+  var el = document.getElementById("crt_conf");
+  const cur_width = Number((el.style.width).replace("%", ""));
+  const factor = (1600 / screen.width);
+  el.style.width = (cur_width * factor).toString().concat("%");
+  const add = localStorage.getItem("acc");
+  if (add == null){
+    window.location.href = "./";
+  }
+}
+window.load_create = load_create;
+
+function isLegalStr(s){
+  if (s == ""){
+    return true;
+  }
+  else {
+    return /^[a-zA-Z0-9?!\.]+$/.test(s);
+  }
+
+}
+
+async function confirm_create(){
+  const b = document.getElementById("pben").value;
+  const a = document.getElementById("pamt").value;
+  const ma = document.getElementById("pmat").value;
+  const ms = document.getElementById("pmes").value;
+  var el = document.getElementById("res_box");
+  if (b == ""){
+    el.textContent = "Beneficiary must be a valid address";
+    return;
+  }
+  if (isNaN(parseInt(a)) || parseInt(a) != parseFloat(a)){
+    el.textContent = "Amount needs to be a whole number";
+    return;
+  }
+  else {
+    if (parseInt(a) > 10000){
+      el.textContent = "Amount needs to be smaller than 10000";
+      return;
+    }
+  }
+  if (isNaN(parseInt(ma)) || parseInt(ma) != parseFloat(ma)){
+    el.textContent = "Maturity needs to be a whole number";
+    return;
+  }
+  else {
+    if (parseInt(ma) > 3000){
+      el.textContent = "Maturity must be lesser than 3000 days";
+      return;
+    }
+  }
+  if (!isLegalStr(ms)){
+    el.textContent = "Message can be blank or must be made of letters, numbers and .!?";
+    return;
+  }
+  if (ms.length > 50){
+    el.textContent = "Message cannot be longer than 50 characters";
+    return;
+  }
+
+  const web3 = new Web3(window.ethereum);
+  const abiInstance = ABI.abi;
+  const contract = new web3.eth.Contract(
+                                    abiInstance,
+                     "0xd724613c90224c502D4b2cD5742F56681039edaB");
+
+  const myAddress = localStorage.getItem("acc");
+  if (myAddress == null){
+    el.textContent = "You are not logged in!";
+    return;
+  }
+
+  const pay = web3.utils.toWei('0.01', 'ether');
+
+  try {
+    const res = await contract.methods.storeAmount(b, ma, a, ms).send({from: myAddress, value: a * pay, gas: 100000, gasLimit: 1000000});
+    el.textContent = "Capsule Created successfully! Create another if you want using the same form";
+    document.getElementById("pben").value = "";
+    document.getElementById("pamt").value = "";
+    document.getElementById("pmat").value = "";
+    document.getElementById("pmes").value = "";
+  }
+  catch (err){
+    console.log(err);
+    el.textContent = "Capsule Creation failed! Make sure the address is real and you have the funds.";
+  }
+
+}
+window.confirm_create = confirm_create;
 
 async function load_capsules(){
   const web3 = new Web3(window.ethereum);
@@ -266,6 +363,158 @@ async function load_capsules(){
 }
 window.load_capsules = load_capsules;
 
+
+async function sort_capsules_amt(){
+  const web3 = new Web3(window.ethereum);
+  const abiInstance = ABI.abi;
+  const contract = new web3.eth.Contract(
+                                    abiInstance,
+                     "0xd724613c90224c502D4b2cD5742F56681039edaB");
+
+  const myAddress = localStorage.getItem("acc");
+  var arr = [];
+
+  try {
+    var res1 = await contract.methods.fetchFutureSenders().call({from: myAddress});
+    var res2 = await contract.methods.fetchFutureAmounts().call({from: myAddress});
+    var res3 = await contract.methods.fetchFutureMaturities().call({from: myAddress});
+    var res4 = await contract.methods.fetchFutureMessages().call({from: myAddress});
+    var i = 0;
+    if ((res1.length != res3.length) || (res1.length != res2.length) || (res3.length != res4.length)){
+      throw "data corruption error";
+      return;
+    }
+
+    while (i < res1.length){
+      arr.push([res1[i], res2[i], res3[i], res4[i]]);
+      i++;
+    }
+
+  }
+  catch (err){
+    console.log(err);
+    return;
+  }
+
+  const dnow = Math.floor(Date.now() / (60*60*24*1000));
+  var el = document.getElementById("cap_cont");
+  el.innerHTML = `
+    <tr style="font-size:1.2em;">
+      <td>Sender</td>
+      <td>Amount in 0.01 FLR</td>
+      <td>Time to Maturity</td>
+      <td>Message</td>
+    </tr>
+  `;
+  arr = arr.sort(sort_amount).reverse();
+  var j = 0;
+  while (j < arr.length){
+    var el = document.getElementById("cap_cont");
+    var tleft = (parseInt(arr[j][2]) - dnow).toString();
+    var val = arr[j][0].concat(" ").concat(arr[j][1]).concat(" ").concat(tleft.concat(" days")).concat(" ").concat(arr[j][3]);
+    if (j % 2 == 0){
+      el.innerHTML = el.innerHTML + `
+      <tr style="background-color: black;color: #ff8c00;">
+          <td>`.concat(arr[j][0]).concat(`</td>
+          <td>`.concat(arr[j][1]).concat(`</td>
+          <td>`.concat(tleft.concat(" days")).concat(`</td>
+          <td>`.concat(arr[j][3]).concat(`</td>
+        </tr>`))));
+    }
+    else {
+      el.innerHTML = el.innerHTML + `
+      <tr style="color: black;background-color: #ff8c00;">
+          <td>`.concat(arr[j][0]).concat(`</td>
+          <td>`.concat(arr[j][1]).concat(`</td>
+          <td>`.concat(tleft.concat(" days")).concat(`</td>
+          <td>`.concat(arr[j][3]).concat(`</td>
+        </tr>`))));
+    }
+
+    j++;
+  }
+
+
+
+}
+window.sort_capsules_amt = sort_capsules_amt;
+
+
+async function sort_capsules_mat(){
+  const web3 = new Web3(window.ethereum);
+  const abiInstance = ABI.abi;
+  const contract = new web3.eth.Contract(
+                                    abiInstance,
+                     "0xd724613c90224c502D4b2cD5742F56681039edaB");
+
+  const myAddress = localStorage.getItem("acc");
+  var arr = [];
+
+  try {
+    var res1 = await contract.methods.fetchFutureSenders().call({from: myAddress});
+    var res2 = await contract.methods.fetchFutureAmounts().call({from: myAddress});
+    var res3 = await contract.methods.fetchFutureMaturities().call({from: myAddress});
+    var res4 = await contract.methods.fetchFutureMessages().call({from: myAddress});
+    var i = 0;
+    if ((res1.length != res3.length) || (res1.length != res2.length) || (res3.length != res4.length)){
+      throw "data corruption error";
+      return;
+    }
+
+    while (i < res1.length){
+      arr.push([res1[i], res2[i], res3[i], res4[i]]);
+      i++;
+    }
+
+  }
+  catch (err){
+    console.log(err);
+    return;
+  }
+
+  const dnow = Math.floor(Date.now() / (60*60*24*1000));
+  var el = document.getElementById("cap_cont");
+  el.innerHTML = `
+    <tr style="font-size:1.2em;">
+      <td>Sender</td>
+      <td>Amount in 0.01 FLR</td>
+      <td>Time to Maturity</td>
+      <td>Message</td>
+    </tr>
+  `;
+  arr = arr.sort(sort_time);
+  var j = 0;
+  while (j < arr.length){
+    var el = document.getElementById("cap_cont");
+    var tleft = (parseInt(arr[j][2]) - dnow).toString();
+    var val = arr[j][0].concat(" ").concat(arr[j][1]).concat(" ").concat(tleft.concat(" days")).concat(" ").concat(arr[j][3]);
+    if (j % 2 == 0){
+      el.innerHTML = el.innerHTML + `
+      <tr style="background-color: black;color: #ff8c00;">
+          <td>`.concat(arr[j][0]).concat(`</td>
+          <td>`.concat(arr[j][1]).concat(`</td>
+          <td>`.concat(tleft.concat(" days")).concat(`</td>
+          <td>`.concat(arr[j][3]).concat(`</td>
+        </tr>`))));
+    }
+    else {
+      el.innerHTML = el.innerHTML + `
+      <tr style="color: black;background-color: #ff8c00;">
+          <td>`.concat(arr[j][0]).concat(`</td>
+          <td>`.concat(arr[j][1]).concat(`</td>
+          <td>`.concat(tleft.concat(" days")).concat(`</td>
+          <td>`.concat(arr[j][3]).concat(`</td>
+        </tr>`))));
+    }
+
+    j++;
+  }
+
+
+
+}
+window.sort_capsules_mat = sort_capsules_mat;
+
 // fetches a registered smart contract ABI from the flare explorer
 // doesn't require a metamask connection
 async function fetchAbi(){
@@ -275,4 +524,64 @@ async function fetchAbi(){
   const response = await fetch(base_url + params);
   const abi = JSON.parse((await response.json())["result"]);
   return abi;
+}
+
+function sort_time(a, b) {
+  var x;
+  var y;
+  const dnow = Math.floor(Date.now() / (60*60*24*1000));
+  if (isNumeric(parseInt(a[2]))){
+    x = parseInt(a[2]);
+  }
+  else {
+    //console.log(parseInt(a[1]));
+    x = 0
+  }
+  if (isNumeric(parseInt(b[2]))){
+    y = parseInt(b[2]);
+  }
+  else {
+    y = 0
+  }
+  console.log(x);
+  console.log(y);
+  if (x === y) {
+      return 0;
+  }
+  else {
+      return (x < y) ? -1 : 1;
+  }
+}
+
+//pts = pts.sort(sorter).reverse();
+function sort_amount(a, b) {
+  var x;
+  var y;
+  if (isNumeric(parseInt(a[1]))){
+    x = parseInt(a[1]);
+  }
+  else {
+    x = 0
+  }
+
+  if (isNumeric(parseInt(b[1]))){
+    y = parseInt(b[1]);
+  }
+  else {
+    y = 0
+  }
+  if (x === y) {
+      return 0;
+  }
+  else {
+      return (x < y) ? -1 : 1;
+  }
+}
+
+
+function isNumeric(str) {
+  //console.log(typeof str);
+  if (typeof str != "string" && typeof str != "number") return false // we only process strings!
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
